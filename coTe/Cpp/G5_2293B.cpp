db9320a7feb84b09f89e4a3ac3c57acc90464058
Luke_YS <yjsmk0902@gmail.com>
2023-04-05 09:25:38 +0900

    백준 G5_2293
    동전 1

---
+++ coTe/Cpp/G5_2293B.cpp
@@ -0,0 +1,28 @@
+//#G5-2293 [동전 1]
+//유형 - DP
+//  
+
+#include<iostream>
+using namespace std;
+
+int n, k;
+int coin[101];
+int DP[10001];
+int main() {
+	cin.tie(NULL);	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		cin >> coin[i];
+	}
+    
+	DP[0] = 1;
+	for (int i = 1; i <= n; i++) {
+		for (int j = coin[i]; j <= k; j++) {
+			DP[j] = DP[j] + DP[j - coin[i]];
+		}
+	}
+	cout << DP[k];
+	return 0;
+}
