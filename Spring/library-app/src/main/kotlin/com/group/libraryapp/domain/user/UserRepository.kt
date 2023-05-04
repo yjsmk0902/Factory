97a0a7811bf65bed3668977303f55bb6a89c4382
Luke_YS <yjsmk0902@gmail.com>
2023-05-04 12:10:21 +0900

    Spring - Java to Kotlin [레포 변경]

---
+++ Spring/library-app/src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.group.libraryapp.domain.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+import javax.swing.text.html.Option
+
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByName(name:String) :Optional<User>
+}
