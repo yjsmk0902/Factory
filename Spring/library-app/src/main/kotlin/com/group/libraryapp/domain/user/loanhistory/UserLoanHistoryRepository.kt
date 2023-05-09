ace6b321aafa68e7ea42e7f2c96292c64863d0f7
Luke_YS <yjsmk0902@gmail.com>
2023-05-09 15:01:21 +0900

    Spring - Java to Kotlin [책 통계 기능]

--- Spring/library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ Spring/library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
-    fun findByBookNameAndStatus(bookName:String, status: UserLoanStatus) :UserLoanHistory?
+    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
+    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
+    fun countByStatus(status: UserLoanStatus): Long
 }
