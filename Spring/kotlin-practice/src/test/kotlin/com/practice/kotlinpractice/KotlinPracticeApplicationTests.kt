f397b9bb82dedfcffd273d85b3205cea00481964
Luke_YS <yjsmk0902@gmail.com>
2023-05-10 10:26:57 +0900

    Board Project 세팅

---
+++ Spring/kotlin-practice/src/test/kotlin/com/practice/kotlinpractice/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.kotlinpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
