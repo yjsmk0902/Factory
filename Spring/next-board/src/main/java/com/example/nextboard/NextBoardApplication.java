cf5e55ce14ebf9ac2d19436fadedeec042371b57
Luke_YS <yjsmk0902@gmail.com>
2023-05-17 11:31:38 +0900

    NEXT Board Project - 초기 세팅

---
+++ Spring/next-board/src/main/java/com/example/nextboard/NextBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextBoardApplication.class, args);
+	}
+
+}
---
+++ Spring/next-board/src/test/java/com/example/nextboard/NextBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
