fcc225417efbafa643e741753e798be1d7433b79
Luke_YS <yjsmk0902@gmail.com>
2023-05-20 03:39:57 +0900

    NEXT Board Project - PostService 수정
    게시글 수정 로직 추가
    게시글 검색 로직 (제목 / 작성자) 추가
    내가 쓴 게시글 뷰 로직 추가
    카테고리별 게시글 뷰 로직 추가

--- Spring/next-board/src/main/java/com/example/nextboard/web/post/PostService.java
+++ Spring/next-board/src/main/java/com/example/nextboard/web/post/PostService.java
@@ -1,14 +1,21 @@
 package com.example.nextboard.web.post;
 
+import com.example.nextboard.domain.member.repository.MemberRepository;
 import com.example.nextboard.domain.post.dto.request.CreatePostRequestDto;
 import com.example.nextboard.domain.member.entity.Member;
+import com.example.nextboard.domain.post.dto.request.ModifyPostRequestDto;
+import com.example.nextboard.domain.post.dto.response.CompactPostResponseDto;
 import com.example.nextboard.domain.post.dto.response.PostResponseDto;
+import com.example.nextboard.domain.post.entity.Category;
 import com.example.nextboard.domain.post.entity.Post;
 import com.example.nextboard.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +24,53 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-    //게시판 생성 로직
+    //게시글 생성 로직
     @Transactional
     public void createPost(@RequestBody CreatePostRequestDto createPostRequestDto, Member member) {
         postRepository.save(createPostRequestDto.toEntity(member));
     }
 
-    //게시판 삭제 로직
+    //게시글 삭제 로직
     @Transactional
     public void deletePost(Long postId) {
         postRepository.delete(findPost(postId));
     }
 
-    //게시판 뷰 로직
+    //게시글 수정 로직
+    @Transactional
+    public void modifyPost(@RequestBody ModifyPostRequestDto modifyPostRequestDto, Long postId) {
+        findPost(postId).modifyPost(
+                modifyPostRequestDto.getTitle(),
+                modifyPostRequestDto.getContent(),
+                modifyPostRequestDto.getCategory()
+        );
+    }
+
+    //게시글 검색 로직 (제목으로 검색)
+    public Page<CompactPostResponseDto> getSearchTitle(String keyword, Pageable pageable) {
+        return postRepository.findByTitleContaining(keyword, pageable)
+                .map(CompactPostResponseDto::toDto);
+    }
+
+    //게시글 검색 로직 (작성자로 검색)
+    public Page<CompactPostResponseDto> getSearchMember(String keyword, Pageable pageable) {
+        return postRepository.findByMember_NameContaining(keyword, pageable)
+                .map(CompactPostResponseDto::toDto);
+    }
+
+    //내가 쓴 게시글 뷰 로직
+    public Page<CompactPostResponseDto> getMyPost(Member member, Pageable pageable) {
+        return postRepository.findByMemberOrderByIdDesc(member, pageable)
+                .map(CompactPostResponseDto::toDto);
+    }
+
+    //카테고리별 게시글 뷰 로직
+    public Page<CompactPostResponseDto> getCategoryPost(Category category, Pageable pageable) {
+        return postRepository.findByCategoryOrderByIdDesc(category, pageable)
+                .map(CompactPostResponseDto::toDto);
+    }
+
+    //게시글 상세 뷰 로직
     public PostResponseDto viewPost(Long postId) {
         return PostResponseDto.toDto(findPost(postId));
     }
@@ -37,4 +78,5 @@ public class PostService {
     public Post findPost(Long postId) {
         return postRepository.findById(postId).orElseThrow(RuntimeException::new);     //예외 처리 필요
     }
+
 }
