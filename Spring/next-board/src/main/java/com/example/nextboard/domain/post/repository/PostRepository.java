534657827cff502bec661dbfa91605fa4e0475f9
Luke_YS <yjsmk0902@gmail.com>
2023-05-20 03:37:55 +0900

    NEXT Board Project - PostRepository 수정
    검색 로직을 위한 쿼리 메소드 추가

--- Spring/next-board/src/main/java/com/example/nextboard/domain/post/repository/PostRepository.java
+++ Spring/next-board/src/main/java/com/example/nextboard/domain/post/repository/PostRepository.java
@@ -1,7 +1,20 @@
 package com.example.nextboard.domain.post.repository;
 
+import com.example.nextboard.domain.member.entity.Member;
+import com.example.nextboard.domain.post.entity.Category;
 import com.example.nextboard.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByMemberOrderByIdDesc(Member member, Pageable pageable);
+
+    Page<Post> findByCategoryOrderByIdDesc(Category category, Pageable pageable);
+
+    Page<Post> findByTitleContaining(String keyword, Pageable pageable);
+
+    Page<Post> findByMember_NameContaining(String keyword, Pageable pageable);
+
 }
