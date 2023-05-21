a3c1f2241d4cf15f85d5c3b3d7cd53eb4ad88e3a
Luke_YS <yjsmk0902@gmail.com>
2023-05-21 20:14:31 +0900

    NEXT Board Project - PostRepository 수정
    테스트용 쿼리 메소드 추가

--- Spring/next-board/src/main/java/com/example/nextboard/domain/post/repository/PostRepository.java
+++ Spring/next-board/src/main/java/com/example/nextboard/domain/post/repository/PostRepository.java
@@ -7,11 +7,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
 
     Page<Post> findByMemberOrderByIdDesc(Member member, Pageable pageable);
 
     Page<Post> findByCategoryOrderByIdDesc(Category category, Pageable pageable);
+
+    List<Post> findByTitleContaining(String keyword);
 
     Page<Post> findByTitleContaining(String keyword, Pageable pageable);
 
