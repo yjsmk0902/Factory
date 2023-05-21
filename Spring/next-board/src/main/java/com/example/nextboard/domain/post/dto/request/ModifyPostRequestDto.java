b720c42580824d33074884af00f65a3e9892229f
Luke_YS <yjsmk0902@gmail.com>
2023-05-21 20:13:59 +0900

    NEXT Board Project - ModifyPostRequestDto

--- Spring/next-board/src/main/java/com/example/nextboard/domain/post/dto/request/ModifyPostRequestDto.java
+++ Spring/next-board/src/main/java/com/example/nextboard/domain/post/dto/request/ModifyPostRequestDto.java
@@ -2,12 +2,14 @@ package com.example.nextboard.domain.post.dto.request;
 
 import com.example.nextboard.domain.post.entity.Category;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ModifyPostRequestDto {
     String title;
     String content;
