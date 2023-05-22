0f4f934bf93b99adbb1281280896c04e7426631a
Luke_YS <yjsmk0902@gmail.com>
2023-05-23 08:56:33 +0900

    NEXT Board Project - CommentService 수정
    댓글 뷰 로직 추가

--- Spring/next-board/src/main/java/com/example/nextboard/web/comment/CommentService.java
+++ Spring/next-board/src/main/java/com/example/nextboard/web/comment/CommentService.java
@@ -5,10 +5,9 @@ import com.example.nextboard.domain.comment.repository.CommentRepository;
 import com.example.nextboard.domain.post.entity.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.awt.print.Pageable;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +16,9 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-    //댓글 뷰 로직
-    public Page<CommentResponseDto> viewComment(Post post, Pageable) {
-        return
+    //댓글 뷰 로직 (페이징)
+    public Page<CommentResponseDto> viewComment(Post post, Pageable pageable) {
+        return commentRepository.findByPostOrderByIdDesc(post, pageable)
+                .map(CommentResponseDto::toDto);
     }
 }
