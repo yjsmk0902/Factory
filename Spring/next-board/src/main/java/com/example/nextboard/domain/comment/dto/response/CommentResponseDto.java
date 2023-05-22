199f52388beb1b7df5445b4cbb68c34ebe6acc73
Luke_YS <yjsmk0902@gmail.com>
2023-05-23 08:46:53 +0900

    NEXT Board Project - CommentResponseDto 추가

---
+++ Spring/next-board/src/main/java/com/example/nextboard/domain/comment/dto/response/CommentResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.nextboard.domain.comment.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommentResponseDto {
+    private String content;
+    private String date;
+}
