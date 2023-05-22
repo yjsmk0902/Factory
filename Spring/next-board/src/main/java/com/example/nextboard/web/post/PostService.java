7b439db3e93a7c56328c35cd208c35af640db3b1
Luke_YS <yjsmk0902@gmail.com>
2023-05-23 08:57:11 +0900

    NEXT Board Project - PostService 수정
    로그 어노테이션 제거

--- Spring/next-board/src/main/java/com/example/nextboard/web/post/PostService.java
+++ Spring/next-board/src/main/java/com/example/nextboard/web/post/PostService.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-@Slf4j
 public class PostService {
 
     private final PostRepository postRepository;
