0188ac6e7f27fffda2cd0dd49dc113cc33a2829a
Luke_YS <yjsmk0902@gmail.com>
2023-05-23 08:56:09 +0900

    NEXT Board Project - CommentRepository 수정
    findByPostOrderByIdDesc 쿼리 메소드 추가

--- Spring/next-board/src/main/java/com/example/nextboard/domain/comment/repository/CommentRepository.java
+++ Spring/next-board/src/main/java/com/example/nextboard/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package com.example.nextboard.domain.comment.repository;
 
 import com.example.nextboard.domain.comment.entity.Comment;
+import com.example.nextboard.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @EntityGraph(attributePaths = "post")
+    Page<Comment> findByPostOrderByIdDesc(Post post, Pageable pageable);
 }
