4e76e683276535f023153334d12537e45ca75f5e
Luke_YS <yjsmk0902@gmail.com>
2023-05-23 12:04:22 +0900

    게시판 프로젝트 연습 - 시작 세팅

---
+++ Spring/post-example/src/main/java/com/practice/postexample/PostExampleApplication.java
@@ -0,0 +1,13 @@
+package com.practice.postexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostExampleApplication.class, args);
+	}
+
+}
