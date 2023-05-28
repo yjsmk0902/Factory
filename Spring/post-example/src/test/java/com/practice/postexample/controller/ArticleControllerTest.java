6179d3871e7bb30aa9bb6656fbf9a81f7af57405
Luke_YS <yjsmk0902@gmail.com>
2023-05-29 02:53:34 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑아봄

---
+++ Spring/post-example/src/test/java/com/practice/postexample/controller/ArticleControllerTest.java
@@ -0,0 +1,80 @@
+package com.practice.postexample.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    @DisplayName("[View] - [GET] 게시글 리스트 (게시판) 페이지")
+    void get_ArticlesView_ApiTest() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+
+        //then
+    }
+
+    @Test
+    @DisplayName("[View] - [GET] 게시글 상세 페이지")
+    void get_ArticleView_ApiTest() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+
+        //then
+    }
+
+    @Test
+    @DisplayName("[View] - [GET] 게시글 검색 전용 페이지")
+    void get_ArticleSearchView_ApiTest() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+
+        //then
+    }
+
+    @Test
+    @DisplayName("[View] - [GET] 게시글 해시태그 검색 페이지")
+    void get_ArticleHashtagSearchView_ApiTest() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+
+        //then
+    }
+
+}
