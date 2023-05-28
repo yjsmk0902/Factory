4736e0d1f0b10eaa51912e3c4de5c4619fd88d16
Luke_YS <yjsmk0902@gmail.com>
2023-05-29 03:33:50 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러의 핸들러 메소드로 테스트 통과하도록 하게끔 함
    뷰 페이지 뼈대 만듬
    테스트에서 호환 가능한 contentType도 테스트 통과하도록 만듬

--- Spring/post-example/src/test/java/com/practice/postexample/controller/ArticleControllerTest.java
+++ Spring/post-example/src/test/java/com/practice/postexample/controller/ArticleControllerTest.java
@@ -24,7 +24,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
     @Test
     @DisplayName("[View] - [GET] 게시글 리스트 (게시판) 페이지")
     void get_ArticlesView_ApiTest() throws Exception {
@@ -33,7 +32,7 @@ class ArticleControllerTest {
         //when
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
 
@@ -49,7 +48,7 @@ class ArticleControllerTest {
         //when
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -68,7 +67,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
 
         //then
     }
@@ -83,7 +82,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
 
         //then
     }
