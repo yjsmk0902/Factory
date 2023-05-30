0d0aa39ad875815d55f9d755ee95ed955f6caace
Luke_YS <yjsmk0902@gmail.com>
2023-05-30 11:10:26 +0900

    #16 - 로그인 컨트롤러 테스트 + Security 설정 반영
    Spring Security가 생성해준 로그인 컨트롤러 테스트
    시큐리티 설정을 테스트에 사용할 수 있도록 테스트에 적용

---
+++ Spring/post-example/src/test/java/com/practice/postexample/controller/AuthControllerTest.java
@@ -0,0 +1,38 @@
+package com.practice.postexample.controller;
+
+import com.practice.postexample.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 인증")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    @DisplayName("[View] - [GET] 로그인 페이지")
+    void tryLogIn_ApiTest() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+
+        //then
+    }
+}
