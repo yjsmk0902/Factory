171394aeb4b0452aaabcd99a1b124a180e4b7fc4
Luke_YS <yjsmk0902@gmail.com>
2023-05-31 15:38:33 +0900

    #11 - JpaRepositoryTest 수정
    도메인 수정에 따라 코드를 일부 수정함
    @Builder추가로 of메소드를 수정함

--- Spring/post-example/src/test/java/com/practice/postexample/repository/JpaRepositoryTest.java
+++ Spring/post-example/src/test/java/com/practice/postexample/repository/JpaRepositoryTest.java
@@ -3,6 +3,7 @@ package com.practice.postexample.repository;
 import com.practice.postexample.config.JpaConfig;
 import com.practice.postexample.domain.Article;
 import com.practice.postexample.domain.ArticleComment;
+import com.practice.postexample.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,13 +21,16 @@ class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     public JpaRepositoryTest(
             @Autowired ArticleRepository articleRepository,
-            @Autowired ArticleCommentRepository articleCommentRepository
+            @Autowired ArticleCommentRepository articleCommentRepository,
+            @Autowired UserAccountRepository userAccountRepository
     ) {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @Test
@@ -42,13 +46,26 @@ class JpaRepositoryTest {
         assertThat(articles).isNotNull().hasSize(100);
         assertThat(articleComments).isNotNull().hasSize(1000);
     }
-    
+
     @Test
     @DisplayName("INSERT Test")
-    void insertTest(){
+    void insertTest() {
         //given
         long previousCount = articleRepository.count();
-        Article article = Article.of("new article", "new content", "#spring");
+        UserAccount userAccount = userAccountRepository.save(
+                UserAccount.builder()
+                        .userId("luke")
+                        .userPassword("pw")
+                        .email(null)
+                        .nickname(null)
+                        .memo(null)
+                        .build());
+        Article article = Article.builder()
+                .userAccount(userAccount)
+                .title("new article")
+                .content("new content")
+                .hashtag("new hashtag")
+                .build();
 
         //when
         Article savedArticle = articleRepository.save(article);
@@ -59,7 +76,7 @@ class JpaRepositoryTest {
 
     @Test
     @DisplayName("UPDATE Test")
-    void updateTest(){
+    void updateTest() {
         //given
         Article article = articleRepository.findById(1L).orElseThrow();
         String updatedHashtag = "#springboot";
@@ -71,10 +88,10 @@ class JpaRepositoryTest {
         //then
         assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
     }
-    
+
     @Test
     @DisplayName("DELETE Test")
-    void deleteTest(){
+    void deleteTest() {
         //given
         Article article = articleRepository.findById(1L).orElseThrow();
         long previousArticleCount = articleRepository.count();
