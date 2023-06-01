4f83668bd92ba36f73ff29eda1025459487c5f30
Luke_YS <yjsmk0902@gmail.com>
2023-06-01 14:05:58 +0900

    #11 - ArticleService 구현
    ArticleServiceTest 를 바탕으로 기존에 껍데기만 있던 ArticleService의 비즈니스 로직을 구현
    
    ArticleServiceTest 의 모든 테스트 코드 통과 확인

--- Spring/post-example/src/main/java/com/practice/postexample/service/ArticleService.java
+++ Spring/post-example/src/main/java/com/practice/postexample/service/ArticleService.java
@@ -1,45 +1,73 @@
 package com.practice.postexample.service;
 
+import com.practice.postexample.domain.Article;
 import com.practice.postexample.domain.type.SearchType;
-import com.practice.postexample.dto.ArticleCommentDto;
-import com.practice.postexample.dto.ArticleDto;
+import com.practice.postexample.dto.article.ArticleDto;
+import com.practice.postexample.dto.article.ArticleWithArticleCommentsDto;
 import com.practice.postexample.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import javax.persistence.EntityNotFoundException;
 
+@Slf4j
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 @Service
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
-    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
-        return Page.empty();
-    }
+    @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return null;
-    }
-    public ArticleDto getArticle(Long articleId) {
-        return null;
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID ->
+                    articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME ->
+                    articleRepository.findByUserAccount_NicknameContaining("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
+    public ArticleWithArticleCommentsDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleWithArticleCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다."));
+    }
+
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
-    @Transactional
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.id());
+            if (dto.title() != null) {
+                article.setTitle(dto.title());
+            }
+            if (dto.content() != null) {
+                article.setContent(dto.content());
+            }
+            article.setHashtag(dto.hashtag());
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다. - dto: {}", dto);
+        }
+
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
-
 
 
 }
